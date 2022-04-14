a98f3e9746429b0b944d48c68366fb21cef673e2
Jasmine Elkins <jasmine.elkins@gmail.com>
2022-04-13 21:32:00 -0400

    All tests passing!

--- app/models/network.rb
+++ app/models/network.rb
@@ -1,3 +1,9 @@
 class Network < ActiveRecord::Base
-  
+  has_many :shows, dependent: :destroy
+
+  def sorry
+    if call_letters.downcase == 'nbc'
+      "We're sorry about passing on John Mulaney's pilot"
+    end
+  end
 end
